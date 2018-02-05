000cc830955b14f33d658f1ef5402d3ce3efb861
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-02-05 21:49:54 +0100

    Rework register form

--- mvc-individual-authentication/Models/AccountViewModels/RegisterViewModel.cs
+++ mvc-individual-authentication/Models/AccountViewModels/RegisterViewModel.cs
@@ -10,17 +10,17 @@ namespace mvc_individual_authentication.Models.AccountViewModels
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "Email", Prompt="Enter Email")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Password", Prompt="Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Confirm password", Prompt="Retype Password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
